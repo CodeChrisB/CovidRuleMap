f1d3746d263904b84d65385266ea0401ecfb7640
Christopher Buchberger <c.buchberger01@gmail.com>
2021-02-14 18:01:17 +0100

    Auto-generated commit

--- assets/Content.ts
+++ assets/Content.ts
@@ -1,6 +1,15 @@
+import { SafeUrl } from "@angular/platform-browser";
+
 export interface IContent{
   "link":string
   "date":string
   "country":string
 }
+
+export interface ISafeContent{
+  "link":SafeUrl
+  "date":string
+  "country":string
+}
+
 
